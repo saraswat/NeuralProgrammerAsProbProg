8557440689bde9444bc199fd13453b2a7aed1713
Vijay Saraswat <vsaraswa@us.ibm.com>
2017-05-14 18:05:47 -0400

    Fixed tests so they pass -- though they leave choicepoints on stack.

--- src/eval.pl
+++ src/eval.pl
@@ -18,6 +18,7 @@ formula(Form,_Table):- formula_value(Form).
 formula(Form, Table):- formula_rows(Form, Table).
 formula(Form, Table):- formula_row(Form, Table).
 
+formula_colName(Col, table(ColNames, _)):- member(Col, ColNames).
 formula_rows(all, _Table).
 formula_rows(either(R,S), Table)       :- formula_rows(R, Table),       formula_rows(S, Table).
 formula_rows(both(R, S), Table)        :- formula_rows(R, Table),       formula_rows(S, Table).
@@ -51,12 +52,12 @@ formula_value([X | Xs]):- formula_value(X), formula_value(Xs).
 % eval_top(Form, Table, Res):- evaluate Form with Table to produce Res, type-checking first.
 % eval(Form, Table, Res):- evaluate Form with Table to produce Res.
 
-eval_top(Form, Table, Res):- formula(Form, Table), eval(Form, Table, Res).
+eval_top(Form, Table, Res):- once(formula(Form, Table)), eval(Form, Table, Res).
 
 % Base case -- normalized indices, values.
 eval(indices(L), _Table, indices(L)).
 eval(val(X),     _Table, val(X)).
-eval(X,          _Table, val(X)):- formula_value(X).
+eval(X,          _Table, val(X)):- once(formula_value(X)).
 
 % 
 eval(all, Table, indices(Res)):- all_rows(Table, Res).
@@ -71,20 +72,10 @@ eval(both(R,S), Table, indices(Res)):-
 	eval(S, Table, indices(IndS)),
 	intersection(IndR, IndS, Res).
 
-eval(card(Rows), Table, val(R)):-
-	eval(Rows, Table, indices(Inds)),
-	length(Inds, R).
-
-eval(first(Rows), Table, indices([Row])):-
-	eval(Rows, Table, indices([Row|_])).
-
-eval(next(Rows), Table, indices(Res)):-
-	eval(Rows, Table, indices(Inds)),
-	next(Inds, Table, Res).
-
-eval(prev(Rows), Table, indices(Res)):-
-	eval(Rows, Table, indices(Inds)),
-	prev(Inds, Res).
+eval(card(Rows),  Table, val(R))        :- eval(Rows, Table, indices(Inds)), length(Inds, R).
+eval(first(Rows), Table, indices([Row])):- eval(Rows, Table, indices([Row|_])).
+eval(next(Rows),  Table, indices(Res))  :- eval(Rows, Table, indices(Inds)), next(Inds, Table, Res).
+eval(prev(Rows),  Table, indices(Res))  :- eval(Rows, Table, indices(Inds)), prev(Inds, Res).
 
 eval(max(ColName, Rows), Table, indices(Res)):-
 	eval(Rows, Table, indices(Inds)),
@@ -96,22 +87,21 @@ eval(min(ColName, Rows), Table, indices(Res)):-
 	col(ColName, Table, J),
 	extrema(min, J, Inds, Table, Res).
 
-
 eval(ge(ColName, Cell, Rows), Table, indices(Res)):- comp(ge, ColName, Cell, Rows, Table, Res).
 eval(gt(ColName, Cell, Rows), Table, indices(Res)):- comp(gt, ColName, Cell, Rows, Table, Res).
 eval(le(ColName, Cell, Rows), Table, indices(Res)):- comp(le, ColName, Cell, Rows, Table, Res).
 eval(lt(ColName, Cell, Rows), Table, indices(Res)):- comp(lt, ColName, Cell, Rows, Table, Res).
 eval(eq(ColName, Cell, Rows), Table, indices(Res)):- comp(eq, ColName, Cell, Rows, Table, Res).
 
-eval(proj(ColName, Rows), Table, Res):-
+eval(proj(ColName, Rows), Table, val(Res)):-
 	eval(Rows, Table, indices(Inds)),
 	col(ColName, Table, J),
 	proj(J, Inds, Table, Res).
 
-eval(V+W, Table, Res):- valop(plus, V, W, Table, Res).
-eval(V*W, Table, Res):- valop(mult, V, W, Table, Res).
-eval(V/W, Table, Res):- valop(divide, V, W, Table, Res).
-eval(V-W, Table, Res):- valop(minus, V, W, Table, Res).
+eval(V+W, Table, val(Res)):- valop(plus, V, W, Table, Res).
+eval(V*W, Table, val(Res)):- valop(mult, V, W, Table, Res).
+eval(V/W, Table, val(Res)):- valop(divide, V, W, Table, Res).
+eval(V-W, Table, val(Res)):- valop(minus, V, W, Table, Res).
 
 valop(Op, L, R, Table, Res):-
 	eval(L, Table, val(L1)),
@@ -126,23 +116,18 @@ comp(Op, ColName, Cell, Rows, Table, Res):-
 
 % Support definitions
 row(I, table(_ColNames, Rows), Row):- arg(I, Rows, Row).
-col(ColName, table(ColNames, _), J):- % J is the index of ColName.
-	functor(ColNames, _, K), 
-	col(ColName, 1, K, ColNames, J).
-col(ColName, I, K, ColNames, J):-
-	arg(I, ColNames, C),
-	(C == ColName
-	-> J=I
-	; (I < K, I1 is I+1, col(ColName, I1, K, ColNames, J))).
+col(ColName, table(ColNames, _), J)  :- 	                  col(ColName, 1, ColNames, J).
+col(ColName, I, [ColName | _Rest], I).
+col(ColName, I, [C | Rest], J)       :- ColName \== C, I1 is I+1, col(ColName, I1, Rest, J).
 
-cell(I, J, TRows, Cell):- arg(I, TRows, Row), arg(J, Row, Cell).
+cell(I, J, TRows, Cell):- nth1(I, TRows, Row), arg(J, Row, Cell).
 
 range(K, K, [K]).
 range(I, K, [I | Res]):- I < K, I1 is I+1, range(I1, K, Res).
 
-all_rows(table(_, TRows), ToK):- functor(TRows, rows, K), range(1, K, ToK).
+all_rows(table(_, TRows), ToK):- length(TRows, K), once(range(1, K, ToK)).
 
-next(Inds, table(_, Rows), Res):- functor(Rows, _, K), next_1(Inds, K, Res).
+next(Inds, table(_, Rows), Res):- length(Rows, K), next_1(Inds, K, Res).
 next_1([], _K, []).
 next_1([I|Inds], K, [I1|Res]):- I < K, I1 is I+1, next_1(Inds, K, Res).
 next_1([I|Inds], K, Res)     :- I >= K,           next_1(Inds, K, Res).
@@ -172,21 +157,21 @@ comp_cell(eq, Cell1, Cell, _I, Res,      Res) :- Cell1 \== Cell.
 
 extrema(Op, J, [I | Inds], table(_, TRows), Res):-
 	cell(I, J, TRows, Cell), 
-	extrema(Op, J, Inds, Cell-[I], TRows, Res).
+	extrema(Op, J, Inds, Cell-[I|Tail]-Tail, TRows, Res).
 
-extrema_cell(max, Cell1, Cell, I, _Is, Cell1-[I])   :- Cell1 > Cell.
-extrema_cell(max, Cell1, Cell, I,  Is, Cell-[I|Is]) :- Cell1 == Cell.
-extrema_cell(max, Cell1, Cell, _I, Is, Cell-Is)     :- Cell1 < Cell.
-extrema_cell(min, Cell1, Cell, _I, Is, Cell-Is)     :- Cell1 > Cell.
-extrema_cell(min, Cell1, Cell, I,  Is, Cell-[I|Is]) :- Cell1 == Cell.
-extrema_cell(min, Cell1, Cell, I, _Is, Cell1-[I])   :- Cell1 < Cell.
+extrema_cell(max, Cell1,  I, Cell-_Is-_Tail,      Cell1-[I|NewTail]-NewTail) :- Cell1 > Cell.
+extrema_cell(max, Cell1,  I, Cell-Is-[I|NewTail], Cell-Is-NewTail)           :- Cell1 == Cell.
+extrema_cell(max, Cell1, _I, Cell-Is-Tail,        Cell-Is-Tail)              :- Cell1 < Cell.
+extrema_cell(min, Cell1, _I, Cell-Is-Tail,        Cell-Is-Tail)              :- Cell1 > Cell.
+extrema_cell(min, Cell1,  I, Cell-Is-[I|NewTail], Cell-Is-NewTail)           :- Cell1 == Cell.
+extrema_cell(min, Cell1,  I, Cell-_Is-_Tail,      Cell1-[I|NewTail]-NewTail) :- Cell1 < Cell.
 
 
-extrema(_Op, _J, [], _Cell-Maxes, _TRows, Maxes).
-extrema(Op, J, [I|Inds], Cell-Maxes, TRows, Res):-
+extrema(_Op, _J, [], _Cell-Maxes-[], _TRows, Maxes).
+extrema(Op, J, [I|Inds], Old, TRows, Res):-
 	cell(I, J, TRows, Cell1),
-	extrema_cell(Op, Cell1, Cell, I, Maxes, New),
-	extreme(Op, J, Inds, New, TRows, Res).
+	extrema_cell(Op, Cell1, I, Old, New),
+	extrema(Op, J, Inds, New, TRows, Res).
 
 
 proj(J, Inds, table(_, TRows), Res):- proj_1(J, Inds, TRows, Res).
--- src/eval_test.pl
+++ src/eval_test.pl
@@ -1,4 +1,4 @@
-table(table([year, city, country, nations],
+table_1(table([year, city, country, nations],
 	    [row(1896, athens, greece, 14),
 	     row(1900, paris, france, 24),
 	     row(1904, 'st louis', usa, 12),
@@ -6,25 +6,28 @@ table(table([year, city, country, nations],
 	     row(2008, beijing, china, 204),
 	     row(2012, london, uk, 204)])).
 :- begin_tests(eval).
+test("Show me all entries."):-
+	table_1(Table),
+	eval(all, Table, indices([1,2,3,4,5,6])).
 test("Greece held its last Summer Olympics in which year?"):-
-	table(Table),
+	table_1(Table),
 	eval(proj(year, max(year, eq(country, greece, all))), Table, val([2004])).
 
 test("In which city’s the first time with at least 20 nations?"):-
-	table(Table),
+	table_1(Table),
 	eval_top(proj(city, min(year, ge(nations, 20, all))), Table, val([paris])).
 
 test("Which years have the most participating countries?"):-
-	table(Table),
+	table_1(Table),
 	eval_top(proj(year, max(nations, all)), Table, val([2008,2012])).
 
 test("How many events were in Athens, Greece?"):-
-	table(Table),
-	eval_top(card(eq(city, athens, all)), Table, val([2])).
+	table_1(Table),
+	eval_top(card(eq(city, athens, all)), Table, val(2)).
 
 test("How many more participants were there in 1900 than in the first year?"):-
-	table(Table),
-	eval_top(proj(nations, eq(year, 1990, all))-proj(nations, min(year, all)), Table, val([10])).
+	table_1(Table),
+	eval_top(proj(nations, eq(year, 1900, all))-proj(nations, min(year, all)), Table, val([10])).
 
 :- end_tests(eval).
 	    
